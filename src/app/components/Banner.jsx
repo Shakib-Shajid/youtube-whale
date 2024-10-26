27e57537e22be193e8150e8ade308da47f8503ca
shakib-shajid <shakibalshajid1682@gmail.com>
2024-10-26 21:05:14 +0600

    banner font and text updated

--- src/app/components/Banner.jsx
+++ src/app/components/Banner.jsx
@@ -13,16 +13,20 @@ const Banner = () => {
                     <div className='flex justify-center mb-5'>
                         <Image src="/logo.svg" width="300" height="300" alt="" />
                     </div>
-                    {/* text-[1.5rem] */}
+                    {/* text-[1.5rem]    custom_320:text-[1.4rem]  custom_412:text-[1.5rem]    */}
                     <h1 className="mb-10 
-                    custom_320:text-[1.4rem] text-[1.4rem]    
-                    custom_412:text-[1.5rem]    
+                    custom_320:text-[1.7rem]  
+                    custom_412:text-[1.7rem]    
                     md:text-[3.1rem] lg:text-[4rem] font-bold leading-tight md:leading-[1.25]">
                         ¿Tus competidores están <br className='hidden lg:grid' /> robando tus clientes?
                     </h1>
 
-                    <p className="mb-5 text-lg leading-6 md:w-[90%] mx-auto lg:w-full">70% de los usuarios hace clic en uno de los primeros cinco resultados de Google <br className='hidden lg:grid' />
-                        Si no inviertes en SEO, ellos se llevan tus oportunidades</p>
+                    <p className="mb-5 text-lg leading-6">70% de los usuarios hace clic en uno de los primeros cinco resultados de Google</p>
+                    <p className="mb-5 text-lg leading-6">Si no inviertes en SEO, ellos se llevan tus oportunidades </p>
+
+
+                    {/* <p className="mb-5 text-lg leading-6 md:w-[90%] mx-auto lg:w-full">70% de los usuarios hace clic en uno de los primeros cinco resultados de Google <br className='hidden lg:grid' />
+                        Si no inviertes en SEO, ellos se llevan tus oportunidades</p> */}
                     <button className="btn bg-[#0556BF] btn-lg rounded-full w-full md:w-[20rem] text-white mb-10 mt-7">Quiero mi análisis SEO GRATIS</button>
                     {/* <p className="mb-5 text-lg leading-6">Or save money with a <span className='text-[#065FD4]'>family or student plan</span>  </p>
 
