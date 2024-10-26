115435845963d249852d3dd5dbf0ec52df712aad
shakib-shajid <shakibalshajid1682@gmail.com>
2024-10-26 18:25:10 +0600

    change logo and text

--- src/app/components/Banner.jsx
+++ src/app/components/Banner.jsx
@@ -9,23 +9,23 @@ const Banner = () => {
                 backgroundImage: "url(https://www.gstatic.com/youtube/img/promos/growth/ytp_lp2_background_web_4098x2304.jpg)",
             }}>
             <div className="hero-content text-center mt-10">
-                <div className=" max-w-xl w-[90%] mx-auto md:w-full">
+                <div className="max-w-2xl w-[90%] mx-auto md:w-full">
                     <div className='flex justify-center mb-5'>
-                        <Image src="/premium.png" width="300" height="300" alt="" />
+                        <Image src="/logo.svg" width="300" height="300" alt="" />
                     </div>
-                    <h1 className="mb-10 text-[2.3rem] md:text-[4rem] font-bold leading-7 md:leading-[1]">
-                        All YouTube.
-                        <span className="block my-5"></span>
-                        No interruptions.
+                    <h1 className="mb-10 text-[2.3rem] md:text-[4rem] font-bold leading-tight md:leading-[1.25]">
+                        ¿Tus competidores están
+                        <span></span>
+                        robando tus clientes?
                     </h1>
 
-                    <p className="mb-5 text-lg leading-6">YouTube and YouTube Music ad-free, offline, and in the background</p>
-                    <p className="mb-5 text-lg leading-6">BDT 239.00/month • Cancel anytime </p>
-                    <button className="btn bg-[#0556BF] btn-lg rounded-full w-full md:w-[15rem] text-white mb-10 mt-7">Get YouTube Premium</button>
-                    <p className="mb-5 text-lg leading-6">Or save money with a <span className='text-[#065FD4]'>family or student plan</span>  </p>
+                    <p className="mb-5 text-lg leading-6 md:w-[84%] mx-auto lg:w-full">El 70% de los usuarios hace clic en uno de los primeros cinco resultados de Google. <br className='hidden lg:grid'/>
+                        Si no inviertes en SEO, ellos se llevan tus oportunidades.</p>
+                    <button className="btn bg-[#0556BF] btn-lg rounded-full w-full md:w-[20rem] text-white mb-10 mt-7">Quiero mi análisis SEO GRATIS</button>
+                    {/* <p className="mb-5 text-lg leading-6">Or save money with a <span className='text-[#065FD4]'>family or student plan</span>  </p>
 
                     <p className='text-xs leading-5'>Recurring billing. <br />
-                        <span className='text-[#065FD4]'> Restrictions apply.</span> </p>
+                        <span className='text-[#065FD4]'> Restrictions apply.</span> </p> */}
                 </div>
             </div>
         </div>
--- src/app/components/Offer.jsx
+++ src/app/components/Offer.jsx
@@ -3,47 +3,47 @@ import React from 'react';
 
 const Offer = () => {
     return (
-        <div className='grid grid-cols-1 lg:grid-cols-4 gap-2 max-w-7xl mx-auto'>
+        <div className='grid grid-cols-1 lg:grid-cols-4 gap-2 max-w-7xl mx-auto md:mt-10'>
             {/* 1st */}
-            <div className="card card-compact w-80 h-full mx-auto ">
+            <div className="card card-compact w-80 h-full mx-auto text-center lg:text-start ">
                 <figure className="w-full h-32 flex justify-center items-center">
                     <Image src="https://www.gstatic.com/youtube/img/promos/growth/premium_lp2_small_feature_Ad-free_light_tablet_600x444.webp" width="100" height="100" alt="" className="object-contain" />
                 </figure>
                 <div className="card-body ">
-                    <p className='text-base leading-[1.38rem]'>Ad-free so you can immerse in your favorite videos without interruption</p>
+                    <p className='text-base leading-[1.38rem]'>Estar en los primeros resultados de búsqueda aumenta la exposición de la empresa</p>
                     
                 </div>
             </div>
 
             {/* 2nd */}
-            <div className="card card-compact w-80 h-full mx-auto">
+            <div className="card card-compact w-80 h-full mx-auto text-center lg:text-start">
                 <figure className="w-full h-32 flex justify-center items-center">
                     <Image src="https://www.gstatic.com/youtube/img/promos/growth/premium_lp2_small_feature_DownloadMultiple_light_tablet_600x508.webp" width="100" height="100" alt="" className="object-contain" />
                 </figure>
                 <div className="card-body ">
-                    <p className='text-base leading-[1.38rem]'>Download videos to watch later when you’re offline or on the go</p>
+                    <p className='text-base leading-[1.38rem]'>Atrae visitantes interesados en los productos o servicios ofrecidos</p>
                     
                 </div>
             </div>
 
             {/* 3rd */}
-            <div className="card card-compact w-80 h-full mx-auto">
+            <div className="card card-compact w-80 h-full mx-auto text-center lg:text-start">
                 <figure className="w-full h-32 flex justify-center items-center">
                     <Image src="https://www.gstatic.com/youtube/img/promos/growth/premium_lp2_small_feature_PictureInPicture_light_tablet_600x764.webp" width="100" height="100" alt="" className="object-contain" />
                 </figure>
                 <div className="card-body ">
-                    <p className='text-base leading-[1.38rem]'>Background play so you can watch while using other apps or with your screen locked</p>
+                    <p className='text-base leading-[1.38rem]'>Los usuarios tienden a confiar más en los sitios bien posicionados</p>
                     
                 </div>
             </div>
 
             {/* 4th */}
-            <div className="card card-compact w-80 h-full mx-auto">
+            <div className="card card-compact w-80 h-full mx-auto text-center lg:text-start">
                 <figure className="w-full h-32 flex justify-center items-center">
                     <Image src="https://www.gstatic.com/youtube/img/promos/growth/premium_lp2_small_feature_UnlimitedMusic_light_tablet_600x505.webp" width="100" height="100" alt="" className="object-contain" />
                 </figure>
                 <div className="card-body ">
-                    <p className='text-base leading-[1.38rem]'>Stream all the music you want to hear, ad-free on the YouTube Music app</p>
+                    <p className='text-base leading-[1.38rem]'> Permite evaluar y ajustar estrategias para maximizar el retorno de inversión</p>
                     
                 </div>
             </div>
