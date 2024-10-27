e70986e8dd3410ce140733d5df081bb486dd4296
shakib-shajid <shakibalshajid1682@gmail.com>
2024-10-27 22:32:26 +0600

    price page text updated

--- src/app/components/Offer.jsx
+++ src/app/components/Offer.jsx
@@ -11,7 +11,7 @@ const Offer = () => {
                 <figure className="w-full h-32 flex justify-center items-center">
                     <Image src="https://www.gstatic.com/youtube/img/promos/growth/premium_lp2_small_feature_Ad-free_light_tablet_600x444.webp" width="100" height="100" alt="" className="object-contain" />
                 </figure>
-                <div className="card-body ">
+                <div className="card-body text-center">
                     <p className='text-base leading-[1.38rem]'>Estar en los primeros resultados de búsqueda aumenta la exposición de la empresa</p>
                     
                 </div>
@@ -22,7 +22,7 @@ const Offer = () => {
                 <figure className="w-full h-32 flex justify-center items-center">
                     <Image src="https://www.gstatic.com/youtube/img/promos/growth/premium_lp2_small_feature_DownloadMultiple_light_tablet_600x508.webp" width="100" height="100" alt="" className="object-contain" />
                 </figure>
-                <div className="card-body ">
+                <div className="card-body text-center">
                     <p className='text-base leading-[1.38rem]'>Atrae visitantes interesados en los productos o servicios ofrecidos</p>
                     
                 </div>
@@ -33,7 +33,7 @@ const Offer = () => {
                 <figure className="w-full h-32 flex justify-center items-center">
                     <Image src="https://www.gstatic.com/youtube/img/promos/growth/premium_lp2_small_feature_PictureInPicture_light_tablet_600x764.webp" width="100" height="100" alt="" className="object-contain" />
                 </figure>
-                <div className="card-body ">
+                <div className="card-body text-center">
                     <p className='text-base leading-[1.38rem]'>Los usuarios tienden a confiar más en los sitios bien posicionados</p>
                     
                 </div>
@@ -44,7 +44,7 @@ const Offer = () => {
                 <figure className="w-full h-32 flex justify-center items-center">
                     <Image src="https://www.gstatic.com/youtube/img/promos/growth/premium_lp2_small_feature_UnlimitedMusic_light_tablet_600x505.webp" width="100" height="100" alt="" className="object-contain" />
                 </figure>
-                <div className="card-body ">
+                <div className="card-body text-center">
                     <p className='text-base leading-[1.38rem]'> Permite evaluar y ajustar estrategias para maximizar el retorno de inversión</p>
                     
                 </div>
--- src/app/components/Price.jsx
+++ src/app/components/Price.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { CiUser } from "react-icons/ci";
+import { FaUser } from "react-icons/fa";
 import { TbUsers } from "react-icons/tb";
 import { RiGraduationCapLine } from "react-icons/ri";
 import { RiShareBoxLine } from "react-icons/ri";
@@ -8,51 +8,87 @@ import { RiShareBoxLine } from "react-icons/ri";
 const Price = () => {
     return (
         <div>
-            <h3 className='text-center text-2xl lg:text-5xl font-bold my-10 lg:my-20'>Pick a membership <br /> that fits you</h3>
+            <h3 className='text-center text-2xl lg:text-5xl font-bold my-10 lg:my-20'>¿Cuál es la opinión de  <br /> nuestros clientes? </h3>
 
             <div className='grid grid-cols-1 lg:grid-cols-3 gap-5 max-w-5xl custom_1080:max-w-5xl xl:max-w-6xl custom_1440:max-w-7xl custom_1600:max-w-7xl lg:max-w-7xl mx-auto'>
                 {/* 1st */}
-                <div className="card bg-base-100 w-72 md:w-80 lg:w-96 xl:w-80 custom_1080:w-[19rem] custom_1440:w-[23rem] custom_1600:w-96 mx-auto shadow-2xl rounded-3xl">
+                <div className="card bg-base-100 w-72 md:w-80 lg:w-96 xl:w-80 custom_1080:w-[19rem] custom_1440:w-[24rem] custom_1600:w-96 mx-auto shadow-2xl rounded-3xl">
                     <div className="card-body p-5">
-                        <h2 className="border-b-2 py-2 pb-4 text-3xl font-bold flex items-center gap-3"><CiUser className='text-2xl'/>Individual</h2>
-                        <div>
+                        <div className='flex gap-4 items-center border-b-2 py-2 pb-4'>
+                            <FaUser className='text-2xl' />
+                            <div className=''>
+                                <h2 className=" text-xl font-bold flex items-center gap-3">
+                                    Laura Martínez
+                                    <br />
+                                </h2>
+                                <p>Directora de Marketing</p>
+                            </div>
+                        </div>
+                        {/* <div>
                             <small className='text-[#606060] font-bold'>Monthly</small>
                             <h5 className='text-lg'>BDT 239.00/month</h5>
                             <small className='text-[#065FD4]'>Restrictions apply.</small>
                         </div>
                         <div className="card-actions justify-end">
                             <button className="btn w-full rounded-full bg-[#0556BF] text-white text-lg">Get YouTube Premium</button>
-                        </div>
+                        </div> */}
+                        <p>Desde que contratamos a Whale Lagoon, hemos visto una mejora significativa en nuestra posición
+                            frente a la competencia. Su análisis detallado y estrategias personalizadas nos han permitido
+                            superar a nuestros principales competidores. ¡Altamente recomendados!</p>
                     </div>
                 </div>
 
                 {/* 2nd */}
-                <div className="card bg-base-100 w-72 md:w-80 lg:w-96 xl:w-80 custom_1080:w-[19rem] custom_1440:w-[23rem] custom_1600:w-96 mx-auto shadow-2xl rounded-3xl">
+                <div className="card bg-base-100 w-72 md:w-80 lg:w-96 xl:w-80 custom_1080:w-[19rem] custom_1440:w-[24rem] custom_1600:w-96 mx-auto shadow-2xl rounded-3xl">
                     <div className="card-body p-5">
-                        <h2 className="border-b-2 py-2 pb-4 text-3xl font-bold flex items-center gap-3">< TbUsers className='text-2xl'/> Family</h2>
-                        <div>
+                        <div className='flex gap-4 items-center border-b-2 py-2 pb-4'>
+                            <FaUser className='text-2xl' />
+                            <div className=''>
+                                <h2 className=" text-xl font-bold flex items-center gap-3">
+                                    Carlos Gómez
+                                    <br />
+                                </h2>
+                                <p>Gerente de Proyectos</p>
+                            </div>
+                        </div>
+                        {/* <div>
                             <small className='text-[#606060] font-bold'>Monthly</small>
                             <h5 className='text-lg'>BDT 449.00/month</h5>
                             <small className='text-[#606060]'>Add up to 5 family members (ages 13+) in your household. <span className='text-[#065FD4]'>Restrictions apply.</span></small>
                         </div>
                         <div className="card-actions justify-end">
                             <button className="btn w-full rounded-full bg-[#b5cbe8] text-[#065FD4] text-lg">Get Family Plan</button>
-                        </div>
+                        </div> */}
+                        <p>Necesitábamos resultados rápidos y Whale Lagoon no nos decepcionó. En tiempo récord,
+                            optimizaron nuestra campaña y logramos un ROI impresionante. Su eficiencia y rapidez son
+                            inigualables.</p>
                     </div>
                 </div>
 
                 {/* 3rd */}
-                <div className="card bg-base-100 w-72 md:w-80 lg:w-96 xl:w-80 custom_1080:w-[19rem] custom_1440:w-[23rem] custom_1600:w-96 mx-auto shadow-2xl rounded-3xl">
+                <div className="card bg-base-100 w-72 md:w-80 lg:w-96 xl:w-80 custom_1080:w-[19rem] custom_1440:w-[24rem] custom_1600:w-96 mx-auto shadow-2xl rounded-3xl">
                     <div className="card-body p-5">
-                        <h2 className="border-b-2 py-2 pb-4 text-3xl font-bold flex items-center gap-3">< RiGraduationCapLine className='text-2xl'/>Student</h2>
-                        <div>
+                        <div className='flex gap-4 items-center border-b-2 py-2 pb-4'>
+                            <FaUser className='text-2xl' />
+                            <div className=''>
+                                <h2 className=" text-xl font-bold flex items-center gap-3">
+                                    Ana Rodríguez
+                                    <br />
+                                </h2>
+                                <p>CEO</p>
+                            </div>
+                        </div>
+                        {/* <div>
                             <small className='text-[#606060] font-bold'>Monthly</small>
                             <h5 className='text-lg'>BDT 139.00/month</h5>
                             <small className='text-[#606060]'>Eligible students only. Annual verification required. <span className='text-[#065FD4]'>Restrictions apply.</span></small>
                         </div>
                         <div className="card-actions justify-end">
-                        <button className="btn w-full rounded-full bg-[#b5cbe8] text-[#065FD4] text-lg"><RiShareBoxLine className='text-2xl'/>Get Student Plan</button>
-                        </div>
+                            <button className="btn w-full rounded-full bg-[#b5cbe8] text-[#065FD4] text-lg"><RiShareBoxLine className='text-2xl' />Get Student Plan</button>
+                        </div> */}
+                        <p>Nuestra empresa estaba en un punto muerto en ventas hasta que empezamos a trabajar con
+                            Whale Lagoon. En pocos meses, nuestras ventas comenzaron a crecer de nuevo gracias a sus
+                            estrategias de SEO y SEM. ¡Estamos muy agradecidos!</p>
                     </div>
                 </div>
             </div>
