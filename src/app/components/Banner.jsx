f6770628a50459c9005a645a2d060cdc1cb59a4b
shakib-shajid <shakibalshajid1682@gmail.com>
2024-10-29 00:45:27 +0600

    calendly link added in button

--- src/app/components/Banner.jsx
+++ src/app/components/Banner.jsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 const Banner = () => {
@@ -27,7 +28,7 @@ const Banner = () => {
 
                     {/* <p className="mb-5 text-lg leading-6 md:w-[90%] mx-auto lg:w-full">70% de los usuarios hace clic en uno de los primeros cinco resultados de Google <br className='hidden lg:grid' />
                         Si no inviertes en SEO, ellos se llevan tus oportunidades</p> */}
-                    <button className="btn bg-[#0556BF] btn-lg rounded-full w-full md:w-[20rem] text-white mb-10 mt-7">Quiero mi análisis SEO GRATIS</button>
+                    <Link href="https://calendly.com/d/cqjr-4dn-xcf/presentacion-analisis-seo?month=2024-10" target='_blank'><button className="btn bg-[#0556BF] btn-lg rounded-full w-full md:w-[20rem] text-white mb-10 mt-7">Quiero mi análisis SEO GRATIS</button></Link>
                     {/* <p className="mb-5 text-lg leading-6">Or save money with a <span className='text-[#065FD4]'>family or student plan</span>  </p>
 
                     <p className='text-xs leading-5'>Recurring billing. <br />
--- src/app/components/Premium.jsx
+++ src/app/components/Premium.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react';
 
 const Premium = () => {
@@ -5,7 +6,7 @@ const Premium = () => {
         <div className='text-center my-20 w-[80%] mx-auto'>
             <h2 className='text-4xl md:text-5xl font-bold'>Analizamos tu SEO gratis </h2>
             <p className='my-10 text-lg leading-6'>Realizaremos un análisis del SEO de tu web para que sepas qué tienes que mejorar</p>
-            <button className="btn bg-[#0556BF] btn-lg rounded-full text-white mb-10 w-full md:w-[15rem]">Análisis SEO</button>
+            <Link href="https://calendly.com/d/cqjr-4dn-xcf/presentacion-analisis-seo" target='_blank'><button className="btn bg-[#0556BF] btn-lg rounded-full text-white mb-10 w-full md:w-[15rem]">Análisis SEO</button></Link>
             {/* <p className='text-xs'>Recurring billing. <br />
                 <span className='text-[#065FD4]'> Restrictions apply.</span> </p> */}
         </div>
