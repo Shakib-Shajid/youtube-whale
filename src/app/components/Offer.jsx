8df159b931e211683e6e32660d674ae61d38c249
shakib-shajid <shakibalshajid1682@gmail.com>
2024-10-28 14:06:20 +0600

    offer page image uodated

--- src/app/components/Offer.jsx
+++ src/app/components/Offer.jsx
@@ -10,7 +10,8 @@ const Offer = () => {
             <div className="card card-compact w-72 md:w-80 lg:w-96 custom_1080:w-[16rem] custom_1440:w-[20rem] custom_1600:w-80 h-full mx-auto text-center lg:text-start ">
                 <figure className="w-full h-32 flex justify-center items-center">
                     {/* <Image src="https://www.gstatic.com/youtube/img/promos/growth/premium_lp2_small_feature_Ad-free_light_tablet_600x444.webp" width="100" height="100" alt="" className="object-contain" /> */}
-                    <Image src="https://www.yourdigitalweb.com/wp-content/uploads/2021/10/seo-gif-resized.gif" width="200" height="200" alt="" className="object-contain" />
+                    {/* <Image src="https://www.yourdigitalweb.com/wp-content/uploads/2021/10/seo-gif-resized.gif" width="200" height="200" alt="" className="object-contain" /> */}
+                    <Image src="/offer_1.png" width="200" height="200" alt="" className="object-contain" />
                 </figure>
                 <div className="card-body text-center">
                     <p className='text-base leading-[1.38rem]'>Estar en los primeros resultados de búsqueda aumenta la exposición de la empresa</p>
@@ -22,7 +23,8 @@ const Offer = () => {
             <div className="card card-compact w-72 md:w-80 lg:w-96 custom_1080:w-[16rem] custom_1440:w-[20rem] custom_1600:w-80 h-full mx-auto text-center lg:text-start">
                 <figure className="w-full h-32 flex justify-center items-center">
                     {/* <Image src="https://www.gstatic.com/youtube/img/promos/growth/premium_lp2_small_feature_DownloadMultiple_light_tablet_600x508.webp" width="100" height="100" alt="" className="object-contain" /> */}
-                    <Image src="https://i.pinimg.com/originals/66/de/ef/66deef6155183f7d2c78b9cd4255786f.gif" width="200" height="200" alt="" className="object-contain" />
+                    {/* <Image src="https://i.pinimg.com/originals/66/de/ef/66deef6155183f7d2c78b9cd4255786f.gif" width="200" height="200" alt="" className="object-contain" /> */}
+                    <Image src="/offer_2.png" width="200" height="200" alt="" className="object-contain" />
                 </figure>
                 <div className="card-body text-center">
                     <p className='text-base leading-[1.38rem]'>Atrae visitantes interesados en los productos o servicios ofrecidos</p>
@@ -34,7 +36,8 @@ const Offer = () => {
             <div className="card card-compact w-72 md:w-80 lg:w-96 custom_1080:w-[16rem] custom_1440:w-[20rem] custom_1600:w-80 h-full mx-auto text-center lg:text-start">
                 <figure className="w-full h-32 flex justify-center items-center">
                     {/* <Image src="https://www.gstatic.com/youtube/img/promos/growth/premium_lp2_small_feature_PictureInPicture_light_tablet_600x764.webp" width="100" height="100" alt="" className="object-contain" /> */}
-                    <Image src="https://indeedseo.com/blog/wp-content/uploads/2023/01/Optimize-Your-Landing-Pages.gif" width="200" height="200" alt="" className="object-contain" />
+                    {/* <Image src="https://indeedseo.com/blog/wp-content/uploads/2023/01/Optimize-Your-Landing-Pages.gif" width="200" height="200" alt="" className="object-contain" /> */}
+                    <Image src="/offer_3.png" width="200" height="200" alt="" className="object-contain" />
                 </figure>
                 <div className="card-body text-center">
                     <p className='text-base leading-[1.38rem]'>Los usuarios tienden a confiar más en los sitios bien posicionados</p>
@@ -46,7 +49,8 @@ const Offer = () => {
             <div className="card card-compact w-72 md:w-80 lg:w-96 custom_1080:w-[16rem] custom_1440:w-[20rem] custom_1600:w-80 h-full mx-auto text-center lg:text-start">
                 <figure className="w-full h-32 flex justify-center items-center">
                     {/* <Image src="https://www.gstatic.com/youtube/img/promos/growth/premium_lp2_small_feature_UnlimitedMusic_light_tablet_600x505.webp" width="100" height="100" alt="" className="object-contain" /> */}
-                    <Image src="https://sunilpratapsingh.com/wp-content/uploads/2023/01/sunil-pratap-singh-seo-consultant.gif" width="200" height="200" alt="" className="object-contain" />
+                    {/* <Image src="https://sunilpratapsingh.com/wp-content/uploads/2023/01/sunil-pratap-singh-seo-consultant.gif" width="200" height="200" alt="" className="object-contain" /> */}
+                    <Image src="/offer_4.png" width="200" height="200" alt="" className="object-contain" />
                 </figure>
                 <div className="card-body text-center">
                     <p className='text-base leading-[1.38rem]'> Permite evaluar y ajustar estrategias para maximizar el retorno de inversión</p>
