fa4536894afe5411a82b244d9db763406a64e35d
shakib-shajid <shakibalshajid1682@gmail.com>
2024-10-29 00:28:27 +0600

    meta title and favicon added

--- src/app/components/Another.jsx
+++ src/app/components/Another.jsx
@@ -4,12 +4,15 @@ import React from 'react';
 const Another = () => {
     return (
         <div>
-            <div className='w-[90%] mx-auto flex flex-col md:flex-row my-10 '>
-                <div
-                    className="hero lg:w-[50%] rounded-t-3xl md:rounded-r-none md:rounded-l-3xl"
-                    style={{
+            <div className='w-[90%] mx-auto flex flex-col lg:flex-row my-10 bg-cover rounded-2xl' style={{
                         backgroundImage: "url(https://www.gstatic.com/youtube/img/promos/growth/premium_lp2_large_feature_MusicModuleSquare_text_background_tablet_1284x1875.jpg)",
                     }}>
+                <div
+                    className="hero lg:w-[50%] rounded-t-3xl md:rounded-r-none md:rounded-l-3xl"
+                    // style={{
+                    //     backgroundImage: "url(https://www.gstatic.com/youtube/img/promos/growth/premium_lp2_large_feature_MusicModuleSquare_text_background_tablet_1284x1875.jpg)",
+                    // }}
+                    >
                     <div className=' text-white w-[80%] px-2 py-10' >
                         <Image src="/logo.svg" width="200" height="200" alt="" />
                         <h3 className='text-xl md:text-4xl leading-snug font-bold my-5'>¿Pero qué son SEO y SEM? </h3>
@@ -27,7 +30,7 @@ const Another = () => {
                     </div>
                 </div>
 
-                <Image src="/another.png" width="4000" height="1000" alt="" className='rounded-b-3xl md:rounded-l-none md:rounded-r-3xl -mt-1 md:-mt-0 -ml-0 md:-ml-1 md:w-[50%]' />
+                <Image src="/another.png" width="4000" height="1000" alt="" className='rounded-b-3xl w-[80%] md:w-[80%] lg:w-[40%] md:h-[21rem] flex my-auto md:rounded-3xl mx-auto' />
             </div>
 
         </div>
